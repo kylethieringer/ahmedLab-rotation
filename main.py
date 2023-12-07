a59bd2e681adbb74f74a8a0968518c5dc25d4a92
Sama Ahmed <samaahmed@Samas-Mac-Studio.local>
2023-12-07 10:33:22 -0800

    tester

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from nifty_roi_extractor import util
 
+def add_time_series():
+    print("My timeserries")
+
 if __name__ == "__main__":
     print("Hello from your main.py top-level runner!")
     util.hello_world()
